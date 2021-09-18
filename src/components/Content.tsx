1f2b295ea2bb9a1d586db1e84b6f291a83a76a6e
Bruno Martins <bruno.martins217@gmail.com>
2021-09-18 03:05:21 -0300

    desafio completo

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,37 @@
-export function Content() {
-  // Complete aqui
+import { MovieCard } from '../components/MovieCard';
+
+interface MovieProps {
+  imdbID: string;
+  Title: string;
+  Poster: string;
+  Ratings: Array<{
+    Source: string;
+    Value: string;
+  }>;
+  Runtime: string;
+}
+
+interface props {
+  selectedGenre: {
+    title: string;
+  }
+  movies: MovieProps[]
+}
+export default function Content(props: props) {
+  const { selectedGenre, movies } = props
+  return (
+    <>
+      <header>
+        <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
+      </header>
+
+      <main>
+        <div className="movies-list">
+          {movies.map(movie => (
+            <MovieCard key={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+          ))}
+        </div>
+      </main>
+    </>
+  )
 }
